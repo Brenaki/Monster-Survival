1bac11269600de402c0af9917142abd61419fc6a
BNK <victor.legat.cerqueira@gmail.com>
2025-09-14 22:08:28 -0300

    add(Colission.Colission2D): adicionado sistema de colisão

---
+++ src/config/colission/Colission2D.java
@@ -0,0 +1,36 @@
+package config.colission;
+
+/** 
+ * 
+ * @author bnk
+*/
+public class Colission2D {
+    private double x;
+    private double y;
+    private double rangeX;
+    private double rangeY;
+
+    public Colission2D(double rangeX, double rangeY) {
+        this.rangeX = rangeX;
+        this.rangeY = rangeY;
+    }
+
+    public boolean rangeMin() {
+        if (this.x > this.rangeX && this.y > this.rangeY) {
+            return true;
+        }
+        return false;
+    }
+
+    // -- Getters --
+    public double getX() { return x; }
+    public double getY() { return y; }
+    public double getrangeX() { return rangeX; }
+    public double getrangeY() { return rangeY; }
+
+    // -- Setters --
+    public void setX(double x) { this.x = x; }
+    public void setY(double y) { this.y = y; }
+    public void setrangeX(double rangeX) { this.rangeX = rangeX; }
+    public void setrangeY(double rangeY) { this.rangeY = rangeY; }
+}
--- src/game/entity/base/Entity.java
+++ src/game/entity/base/Entity.java
@@ -1,5 +1,7 @@
 package game.entity.base;
 
+import config.colission.Colission2D;
+
 /**
 * 
 * @author bnk
@@ -12,8 +14,9 @@ public class Entity {
     private int height;
     private int health;
     private boolean isVisible;
+    private Colission2D colission;
 
-    public Entity(double x, double y, double speed, int width, int height, int health, boolean isVisible) {
+    public Entity(double x, double y, double speed, int width, int height, int health, boolean isVisible, double rangeX, double rangeY) {
         this.x = x;
         this.y = y;
         this.speed = speed;
@@ -21,6 +24,9 @@ public class Entity {
         this.height = height;
         this.health = health;
         this.isVisible = isVisible;
+        this.colission = new Colission2D(rangeX, rangeY);
+        this.colission.setX(x);
+        this.colission.setY(y);
     }
 
     // -- Getters --
@@ -33,11 +39,67 @@ public class Entity {
     public boolean getIsVisible() { return this.isVisible; }
 
     // -- Setters --
-    public void setX(double x) { this.x = x; }
-    public void setY(double y) { this.y = y; }
+    public void setX(double x) { 
+        this.x = x; 
+        this.colission.setX(x);
+    }
+    public void setY(double y) { 
+        this.y = y; 
+        this.colission.setY(y);
+    }
     public void setSpeed(double speed) { this.speed = speed; }
     public void setWidth(int width) { this.width = width; }
     public void setHeight(int height) { this.height = height; }
     public void setHealth(int health) { this.health = health; }
     public void setIsVisible(boolean isVisible) { this.isVisible = isVisible; }
+    
+    // -- Colision Methods --
+    public Colission2D getColission() { return this.colission; }
+    public void setColissionRange(double rangeX, double rangeY) {
+        this.colission.setrangeX(rangeX);
+        this.colission.setrangeY(rangeY);
+    }
+    public boolean checkRangeMin() {
+        return this.colission.rangeMin();
+    }
+    
+    public boolean isCollidingWith(Entity other) {
+        double distance = Math.sqrt(
+            Math.pow(this.getX() - other.getX(), 2) + 
+            Math.pow(this.getY() - other.getY(), 2)
+        );
+        return distance < (this.getColission().getrangeX() + other.getColission().getrangeX());
+    }
+    
+    public void resolveCollision(Entity other) {
+        if (!this.isCollidingWith(other)) {
+            return; // Não há colisão, não precisa fazer nada
+        }
+        
+        // Calcula a distância entre as entidades
+        double dx = other.getX() - this.getX();
+        double dy = other.getY() - this.getY();
+        double distance = Math.sqrt(dx * dx + dy * dy);
+        
+        // Evita divisão por zero
+        if (distance == 0) {
+            distance = 0.1;
+        }
+        
+        // Calcula a distância mínima necessária para não colidir
+        double minDistance = this.getColission().getrangeX() + other.getColission().getrangeX();
+        
+        // Calcula o quanto precisa se afastar
+        double overlap = minDistance - distance;
+        
+        // Normaliza a direção
+        double separationX = (dx / distance) * (overlap / 2);
+        double separationY = (dy / distance) * (overlap / 2);
+        
+        // Move ambas as entidades para se afastarem
+        this.setX(this.getX() - separationX);
+        this.setY(this.getY() - separationY);
+        other.setX(other.getX() + separationX);
+        other.setY(other.getY() + separationY);
+    }
 }
